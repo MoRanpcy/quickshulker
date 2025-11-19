4d5123b9274f547b1e65dc81ec5b9f2b636c344d
MoRanpcy <moranpcy@163.com>
2025-11-19 21:36:14 +0800

    Updated something

--- src/main/java/net/kyrptonaught/quickshulker/client/QuickShulkerModClient.java
+++ src/main/java/net/kyrptonaught/quickshulker/client/QuickShulkerModClient.java
@@ -4,20 +4,18 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.kyrptonaught.kyrptconfig.keybinding.CustomKeyBinding;
-import net.kyrptonaught.kyrptconfig.keybinding.DisplayOnlyKeyBind;
 import net.kyrptonaught.quickshulker.QuickShulkerMod;
-import net.kyrptonaught.quickshulker.api.EnderChestSyncHandler;
+import net.kyrptonaught.quickshulker.event.KeyBindingRegister;
+import net.kyrptonaught.quickshulker.event.ModKeyCallback;
+import net.kyrptonaught.quickshulker.util.EnderChestSyncHandler;
 import net.kyrptonaught.quickshulker.api.RegisterQuickShulkerClient;
 import net.kyrptonaught.quickshulker.network.EnderChestS2CSyncPacket;
 import net.kyrptonaught.quickshulker.network.OpenInventoryPacket;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EnderChestInventory;
 
 @Environment(EnvType.CLIENT)
@@ -25,17 +23,8 @@ public class QuickShulkerModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.START_WORLD_TICK.register(clientWorld -> {
-            if (MinecraftClient.getInstance().currentScreen == null && QuickShulkerMod.getConfig().keybind) {
-                PlayerEntity player = MinecraftClient.getInstance().player;
-                if (getKeybinding().isKeybindPressed() && player != null && !player.isSpectator()) {
-                    if (player.getMainHandStack().isEmpty() && !player.getOffHandStack().isEmpty())
-                        ClientUtil.CheckAndSend(player.getOffHandStack(), 45);
-                    else
-                        ClientUtil.CheckAndSend(player.getMainHandStack(), 36 + player.getInventory().selectedSlot);
-                }
-            }
-        });
+        ClientTickEvents.START_WORLD_TICK.register(ModKeyCallback::onKeyPressed);
+        KeyBindingRegister.register();
 
         PayloadTypeRegistry.playC2S().register(OpenInventoryPacket.OPEN_INV_ID, OpenInventoryPacket.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(OpenInventoryPacket.OPEN_INV_ID, (payload, context) -> {
@@ -58,13 +47,6 @@ public class QuickShulkerModClient implements ClientModInitializer {
         });
 
         FabricLoader.getInstance().getEntrypoints(QuickShulkerMod.MOD_ID + "_client", RegisterQuickShulkerClient.class).forEach(RegisterQuickShulkerClient::registerClient);
-
-        KeyBindingHelper.registerKeyBinding(new DisplayOnlyKeyBind(
-                "key.quickshulker.config.keybinding",
-                "key.categories.quickshulker",
-                getKeybinding(),
-                setKey -> QuickShulkerMod.config.save()
-        ));
     }
 
     public static CustomKeyBinding getKeybinding() {
